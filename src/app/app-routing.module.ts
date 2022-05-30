ab61c8291d37998dac1492a53f09a3a9ca6ae037
Grant Barnes <gfbarnes78@gmail.com>
2022-05-30 18:41:20 -0500

    adding start of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { CalculateComponent } from './pages/calculate/calculate.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'voc/home', component: HomeComponent },
+  { path: 'voc/calculate', component: CalculateComponent },
+  { path: 'voc', redirectTo: '/voc/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { CalculateComponent } from './pages/calculate/calculate.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalculateComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
